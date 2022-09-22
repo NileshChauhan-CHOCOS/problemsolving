7dbbd1b711bc9a0d741365d60b8dafa275176a0f
Nilesh Chauhan <nileshchauhannile@gmail.com>
2022-09-23 00:28:15 +0530

    23-SEP-2022

--- src/com/practice/singleton/Main.java
+++ src/com/practice/singleton/Main.java
@@ -1,2 +1,13 @@
-package com.practice.singleton;public class Main {
+package com.practice.singleton;
+
+import com.practice.singleton.modelcalss.Employee;
+
+public class Main {
+    public static void main(String[] args){
+        Employee emp = Employee.getInstance("ABC", "XYZ");
+        Employee employee = Employee.getInstance("PQR", "LMN");
+        System.out.println(employee.equals(emp));
+        System.out.println(emp.toString());
+        System.out.println(employee.toString());
+    }
 }
--- src/com/practice/singleton/modelcalss/Employee.java
+++ src/com/practice/singleton/modelcalss/Employee.java
@@ -1,2 +1,22 @@
-package com.practice.singleton.modelcalss;public class Employee {
+package com.practice.singleton.modelcalss;
+
+public class Employee {
+    private final String firstName;
+    private final String lastName;
+    private static Employee instance = null;
+    private Employee(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public static Employee getInstance(String firstName, String lastName){
+        if(instance == null){
+            instance = new Employee(firstName, lastName);
+        }
+        return instance;
+    }
+
+    public String toString(){
+        return this.firstName + " " + this.lastName;
+    }
 }
